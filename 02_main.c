6efc9ef57342a3c675435cf464bed1c03daea05c
Abdul Ahad Shaikh <159826781+micky8955@users.noreply.github.com>
2024-07-16 01:35:32 +0500

    Update 02_main.c

--- 02_main.c
+++ 02_main.c
@@ -1,25 +1,3 @@
-// #include <stdio.h>
-
-// int main()
-// {
-//     int p;
-//     float r;
-//     float t;
-
-//     printf("Enter a principle ammount = ");
-//     scanf("%d", &p);
-
-//     printf("Enter a rate of interest = ");
-//     scanf("%f", &r);
-
-//     printf("Enter a time = ");
-//     scanf("%f", &t);
-
-//     printf("The interest rate of given ammount is = %f $", p * r * t);
-
-//     return 0;
-// }
-
 #include <stdio.h>
 
 int main()
@@ -27,13 +5,13 @@ int main()
     int p;
     float r;
     float t;
-
+    // Taking input principal amount 
     printf("Enter a principal amount in dollars: $");
     scanf("%d", &p);
-
+    // Taking input rate of interest 
     printf("Enter a rate of interest (as a decimal, e.g., 0.05 for 5%%): ");
     scanf("%f", &r);
-
+    // Taking input time 
     printf("Enter a time in years: ");
     scanf("%f", &t);
 
